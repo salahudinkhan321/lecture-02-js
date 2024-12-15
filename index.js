303bd6bf6832945bd883016194e048ac8e5ab35b
Salahudin Saani <udinsalah003@gmail.com>
2024-12-15 11:54:38 +0500

    this is function part

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+// const student = 29
+// const myStudent = true
+
+// if(student === 30 || myStudent){
+//     console.log("You are passed",student+3);
+// }else{
+//     console.log("You are Failed");
+// }
+// // 
+
+// const ahmad = 123
+// console.log(ahmad != 123);
+
+
+
+
+
+function collectMarks(){
+    const data = ["I am ahmad", "Salahudin"]
+
+
+
+
+console.log(data);
+
+
+    return data
+}
+
+collectMarks()
+
+
+const collectMarked = function(){
+    const data = ["I am ahmad", "Salahudin"]
+
+
+
+
+    console.log(data);
+    
+    
+        return data
+}
+collectMarked()
+
+
+
+
+
+
+
 
 // Types Of Operators --------------------------------------------------------------------
 
@@ -16,10 +67,10 @@
 
 // Logical Operators ---------------------------------------------------------------------------------
 
-// let age = 18
+// let age = 12
 // let hasLicense = true
 
-// if(age >= 18 && hasLicense){
+// if(age >= 18 || hasLicense){
 //     console.log("You are allowed to drive")
 // } else{
 //     console.log("You can't drive");
@@ -39,29 +90,29 @@
 
 // Function Declaration ---------------------------
 
-function ahmad(){
-    let data = 'I Am Ahmad'
-    console.log(data);
-    return ahmad
-}
-ahmad()
+// function ahmad(){
+//     let data = 'I Am Ahmad'
+//     console.log(data);
+//     return ahmad
+// }
+// ahmad()
 
 // Function Expression ---------------------------------
 
-const salahudin = function(){
-    let data = "I Am Salahudin"
-    console.log(data);
-    return data
-}
-salahudin()
+// const salahudin = function(){
+//     let data = "I Am Salahudin"
+//     console.log(data);
+//     return data
+// }
+// salahudin()
 
 
 // Arrow Function ------------------------------------
 
-const mouse = () => {
-    let data = "I Am Mouse a part of computer"
-    console.log(data);
-    return data
-}
+// const mouse = () => {
+//     let data = "I Am Mouse a part of computer"
+//     console.log(data);
+//     return data
+// }
 
-mouse()
+// mouse()
---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+// let data1 = 4
+// let data2 = "4"
+
+// console.log(data1 === data2)
+
+
+// let valueOne = 321
+// let valueTwo = 444
+
+// console.log(valueOne != valueTwo);
+// console.log(valueOne <= valueTwo);
+// console.log(valueOne >= valueTwo);
+
+
+
+// console.log(ahmad/=23)
+
+// let salahudin = "salahudin@gmail.com"
+// let data = "salahudin@gmail.com"
+
+
+// if(salahudin === data){
+//     console.log("Change the email");
+// }else{
+//     console.log("You are welcome everything is good");
+    
+// }
