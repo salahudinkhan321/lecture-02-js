43164c80fd36cc1950fec450cdaeec013b16967e
Salahudin Saani <udinsalah003@gmail.com>
2024-12-15 10:58:25 +0500

    this is 2nd lecture on javascript

---
+++ index.js
@@ -0,0 +1,67 @@
+
+// Types Of Operators --------------------------------------------------------------------
+
+// Assignment Operators -----------------------------
+
+// let ahmad = 32432;
+// function sum(){
+//     ahmad=-123
+//     console.log(ahmad);
+//     ahmad/=123
+//     console.log(ahmad);
+// }
+
+// console.log(sum());
+
+
+// Logical Operators ---------------------------------------------------------------------------------
+
+// let age = 18
+// let hasLicense = true
+
+// if(age >= 18 && hasLicense){
+//     console.log("You are allowed to drive")
+// } else{
+//     console.log("You can't drive");
+// }
+
+
+// comparison Operators ---------------------------------------------
+
+// console.log(1 == "1");
+// console.log(1 === "1");
+// console.log(1 != "1");
+// console.log(1 !== "1");
+// console.log(123 <= "123");
+// console.log(123 >= "123");
+
+// Functions And Types -----------------------------------------------------------------
+
+// Function Declaration ---------------------------
+
+function ahmad(){
+    let data = 'I Am Ahmad'
+    console.log(data);
+    return ahmad
+}
+ahmad()
+
+// Function Expression ---------------------------------
+
+const salahudin = function(){
+    let data = "I Am Salahudin"
+    console.log(data);
+    return data
+}
+salahudin()
+
+
+// Arrow Function ------------------------------------
+
+const mouse = () => {
+    let data = "I Am Mouse a part of computer"
+    console.log(data);
+    return data
+}
+
+mouse()
